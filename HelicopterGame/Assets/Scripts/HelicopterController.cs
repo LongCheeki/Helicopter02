f82b74ca8441320bd8cb931bd2399e0cc1e3721f
LongCheeki <Longfei.Du-1@student.uts.edu.au>
2026-02-25 18:25:42 +1100

    Add pickup sound effect

--- HelicopterGame/Assets/Scripts/HelicopterController.cs
+++ HelicopterGame/Assets/Scripts/HelicopterController.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class HelicopterController : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed = 5f;
+
+    [Header("Audio")]
+    public AudioClip pickupClip;
+
     private Rigidbody2D rb;
     private Vector2 movement;
+    private AudioSource audioSource;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if (GameManager.Instance != null && GameManager.Instance.isGameOver) return;
+
+        if (GameManager.Instance != null && GameManager.Instance.isGameOver)
+            return;
 
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
@@ -28,16 +37,31 @@ public class HelicopterController : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
+  
         if (other.CompareTag("Soldier") && GameManager.Instance.CanPickUp())
         {
+    
+            if (pickupClip != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(pickupClip);
+            }
+
             GameManager.Instance.PickUpSoldier();
             Destroy(other.gameObject);
 
             Debug.Log("Picked up soldier. In helicopter: " + GameManager.Instance.soldiersInHelicopter);
         }
+
+
+        if (other.CompareTag("Hospital"))
+        {
+            GameManager.Instance.UnloadAtHospital();
+        }
     }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
+ 
         if (collision.gameObject.CompareTag("Tree"))
         {
             GameManager.Instance.GameOver();
